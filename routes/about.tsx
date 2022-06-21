17b06ec3666d75dc1bfe431d81f12965a92ee4ce
asheikh <asim.sardar.sheikh@gmail.com>
2022-06-22 09:46:42 +1000

    Initial commit

---
+++ routes/about.tsx
@@ -0,0 +1,15 @@
+// routes/about.tsx
+
+/** @jsx h */
+import { h } from "preact"
+
+const AboutPage = () => {
+	return (
+		<main>
+		  <h1>About</h1>
+		  <p>This is an about page.</p>
+		</main>
+	)
+}
+
+export default AboutPage
