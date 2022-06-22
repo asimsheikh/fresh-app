a09d6cef697eb5fa5bf9c65d76128627524a9a06
asheikh <asim.sardar.sheikh@gmail.com>
2022-06-22 21:48:39 +1000

    Added new username dynamic route

--- routes/about.tsx
+++ routes/about.tsx
@@ -2,6 +2,15 @@
 
 /** @jsx h */
 import { h } from "preact"
+import { Handlers } from "$fresh/server.ts"
+
+export const handler: Handlers = {
+	async GET(req, ctx) {
+		const resp = await ctx.render()
+		resp.headers.set("X-Custom-Header", "Hello")
+		return resp
+	}
+}
 
 const AboutPage = () => {
 	return (
